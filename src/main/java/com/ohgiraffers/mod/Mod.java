868ca8e7f4abcd44bd01bb600d2e27de6cbcd297
sieun <sieunuies@gmail.com>
2025-05-28 12:02:45 +0900

    Feat: feature mod method (close - #1)

--- src/main/java/com/ohgiraffers/mod/Mod.java
+++ src/main/java/com/ohgiraffers/mod/Mod.java
@@ -1,4 +1,14 @@
 package com.ohgiraffers.mod;
 
 public class Mod {
+    public int caculateDivision(int a, int b) {
+        int result = 0;
+        try {
+            result = a % b;
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
 }
