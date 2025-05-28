f072cbcda9084f44103d2911666549be5ddfc645
pebble0201 <swj06177@naver.com>
2025-05-28 12:55:01 +0900

    Feat: feature division method (close - #8)

--- src/main/java/com/ohgiraffers/run/Application.java
+++ src/main/java/com/ohgiraffers/run/Application.java
@@ -1,4 +1,26 @@
 package com.ohgiraffers.run;
 
+import com.ohgiraffers.division.Division;
+
+import java.util.Scanner;
+
 public class Application {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double num1, num2;
+
+        System.out.println("첫번째 숫자 입력하세요 : ");
+        num1 = sc.nextDouble();
+
+        System.out.println("두번째 숫자 입력하세요 : ");
+        num2 = sc.nextDouble();
+
+        Division division = new Division();
+        System.out.println(division.div(num1,num2));
+
+        }
+
 }
