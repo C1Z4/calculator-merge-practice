a2d29473dd20f2ccff478cd14f70edf80a2d58c4
ddzeun <ddzeun@gmail.com>
2025-05-28 11:56:15 +0900

    Feat: feature multiply method (close - #3)

--- src/main/java/com/ohgiraffers/run/Application.java
+++ src/main/java/com/ohgiraffers/run/Application.java
@@ -1,4 +1,24 @@
 package com.ohgiraffers.run;
 
+import com.ohgiraffers.multiply.Multiply;
+
+import java.util.Scanner;
+
 public class Application {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Multiply multiply = new Multiply();
+
+        System.out.print("첫 번째 정수 입력: ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 정수 입력: ");
+        sc.nextLine();
+        int num2 = sc.nextInt();
+
+        System.out.println("곱셈 결과: " + (multiply.calculateMultiply(num1, num2)));
+
+    }
 }
