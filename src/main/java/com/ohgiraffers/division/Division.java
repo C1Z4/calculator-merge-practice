13894616c1c761dce3d9207abe29830aae381fa7
pebble0201 <swj06177@naver.com>
2025-05-28 12:55:01 +0900

    Feat: feature division method (close - #8)

--- src/main/java/com/ohgiraffers/division/Division.java
+++ src/main/java/com/ohgiraffers/division/Division.java
@@ -1,4 +1,12 @@
 package com.ohgiraffers.division;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Division {
+
+   public static double div(double num1, double num2) {
+        return num1/num2;
+    }
 }
